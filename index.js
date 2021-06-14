b113bf784425b6488a73f166ad3522e4b5275da7
hajarmsheik <hajar.msheik@gmail.com>
2021-06-14 11:27:56 +0300

    step3

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var express = require('express');
- 
+ let date= new Date();
+ let timenow= date.getHours()+":"+date.getMinutes();
 const app = express();
- //listen to port 3000
+ ////////Create a simple express server///////////
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
 );
-////////////////////////////////////////////
+
 app.get('/', (req, res) => {
     res.send('ok!')
   })
-///////////////////////////////////////////
+////////Step 3 - Create an express simple API/////////
+app.get('/test', (req, res) => res.send({status:200, message:"ok"}));
+
+
+app.get('/time', (req, res) => res.send({status:200, message:timenow}));  
+/////////////////////////////////////////
+
